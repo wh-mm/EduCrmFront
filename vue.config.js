3514b009264fd96d4e9f39ad539902b4dd8df415
lizhaojie <597731180@qq.com>
2020-10-27 19:04:19 +0800

    新增

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
     proxy: {
       '/api': {
         //本地服务接口地址
-        target: 'http://localhost',
+        target: 'http://localhost:8080',
         //远程演示服务地址,可用于直接启动项目
         //target: 'https://saber.bladex.vip/api',
         ws: true,
