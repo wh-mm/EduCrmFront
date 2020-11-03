6386f9078b025e1dd28f806f8ac12ce5df30734e
zhupengbo <1311109465@qq.com>
2020-11-03 16:04:38 +0800

    编码匹配

---
+++ src/api/codematching/matching.js
@@ -0,0 +1,50 @@
+import request from '@/router/axios';
+
+export const getList = (current, size, params) => {
+  return request({
+    url: '/api/taocao-codematching/matching/list',
+    method: 'get',
+    params: {
+      ...params,
+      current,
+      size,
+    }
+  })
+}
+
+export const getDetail = (id) => {
+  return request({
+    url: '/api/taocao-codematching/matching/detail',
+    method: 'get',
+    params: {
+      id
+    }
+  })
+}
+
+export const remove = (ids) => {
+  return request({
+    url: '/api/taocao-codematching/matching/remove',
+    method: 'post',
+    params: {
+      ids,
+    }
+  })
+}
+
+export const add = (row) => {
+  return request({
+    url: '/api/taocao-codematching/matching/submit',
+    method: 'post',
+    data: row
+  })
+}
+
+export const update = (row) => {
+  return request({
+    url: '/api/taocao-codematching/matching/submit',
+    method: 'post',
+    data: row
+  })
+}
+
