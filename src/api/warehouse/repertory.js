57cd6f00459963376b276d9b55abb3e0f748a5af
liuhan <122554402@qq.com>
2020-12-15 17:52:27 +0800

    修复Bug

--- src/api/warehouse/repertory.js
+++ src/api/warehouse/repertory.js
@@ -1,7 +1,7 @@
 import request from '@/router/axios';
 import {ERP_WMS_NAME} from '@/const/YueConst';
 
-export const getRepertoryList = (current, size, params,goodsId) => {
+export const getRepertoryList = (current, size, params) => {
   return request({
     url: ERP_WMS_NAME + '/repertory/page',
     method: 'get',
@@ -9,7 +9,6 @@ export const getRepertoryList = (current, size, params,goodsId) => {
       ...params,
       current,
       size,
-      goodsId
     }
   })
 }
